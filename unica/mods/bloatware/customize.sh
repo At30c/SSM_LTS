451b555dc3acda1d6b7f47d3141073852d921d1e
At30c <PabloAtsoc9993@outlook.com>
2026-08-20 00:03:26 -0300

    change y2s4g for y2slte

--- unica/mods/bloatware/customize.sh
+++ unica/mods/bloatware/customize.sh
@@ -3,7 +3,7 @@ SKIPUNZIP=1
 
 # The source firmware lacks these apps only on the Galaxy S20+ ports.
 case "$TARGET_ASSET_PROFILE" in
-    y2s|y2s4g)
+    y2s|y2slte)
         LOG "- Adding required Samsung Clock and Calendar apps"
         ADD_TO_WORK_DIR "$MODPATH" "system" "system/app/ClockPackage" || exit 1
         ADD_TO_WORK_DIR "$MODPATH" "system" "system/app/SamsungCalendar" || exit 1
